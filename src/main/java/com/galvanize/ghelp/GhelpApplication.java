5a2ecf72dbcb0b5e69641e295e2531934806daad
John Armbruster <jaboyak@gmail.com>
2020-02-27 14:30:55 -0700

    first commit

---
+++ src/main/java/com/galvanize/ghelp/GhelpApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.ghelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhelpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galvanize/ghelp/GhelpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.ghelp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhelpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
